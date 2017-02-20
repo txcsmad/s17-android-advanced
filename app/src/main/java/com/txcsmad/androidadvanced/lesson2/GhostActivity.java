9dbeee3a1e46f88999cfb27a0c243ef9972f2f49
Jacob Lubecki <jacoblubecki@gmail.com>
2017-02-20 16:30:00 -0600

    Setup ghost lesson.

---
+++ app/src/main/java/com/txcsmad/androidadvanced/lesson2/GhostActivity.java
@@ -0,0 +1,88 @@
+/* Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.txcsmad.androidadvanced.lesson2;
+
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.widget.TextView;
+
+import com.txcsmad.androidadvanced.R;
+import com.txcsmad.androidadvanced.lesson2.applied.GhostDictionary;
+
+import java.util.Random;
+
+
+public class GhostActivity extends AppCompatActivity {
+    private static final String COMPUTER_TURN = "Computer's turn";
+    private static final String USER_TURN = "Your turn";
+    private GhostDictionary dictionary;
+    private boolean userTurn = false;
+    private Random random = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ghost);
+        AssetManager assetManager = getAssets();
+
+    }
+
+    /**
+     * Handler for the "Reset" button.
+     * Randomly determines whether the game starts with a user turn or a computer turn.
+     *
+     * @return true
+     */
+    public boolean resetGame() {
+        userTurn = random.nextBoolean();
+        TextView text = (TextView) findViewById(R.id.tv_ghost);
+        text.setText("");
+        TextView label = (TextView) findViewById(R.id.gameStatus);
+        if (userTurn) {
+            label.setText(USER_TURN);
+        } else {
+            label.setText(COMPUTER_TURN);
+            computerTurn();
+        }
+        return true;
+    }
+
+    private void computerTurn() {
+        TextView label = (TextView) findViewById(R.id.gameStatus);
+        // Do computer turn stuff then make it the user's turn again
+        userTurn = true;
+        label.setText(USER_TURN);
+    }
+
+    /**
+     * Handler for user key presses.
+     *
+     * @param keyCode
+     * @param event
+     * @return whether the key stroke was handled.
+     */
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        /**
+         **
+         **  YOUR CODE GOES HERE
+         **
+         **/
+        return super.onKeyUp(keyCode, event);
+    }
+}
